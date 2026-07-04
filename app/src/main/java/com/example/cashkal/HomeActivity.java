29c157a113caddf51c1de8bee4ce2c5d77ef8f13
רן ניסים <rannissim22@gmail.com>
2026-07-04 15:46:03 +0300

    add logout helper and temporary home logout

--- app/src/main/java/com/example/cashkal/HomeActivity.java
+++ app/src/main/java/com/example/cashkal/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.example.cashkal;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cashkal.databinding.ActivityHomeBinding;
+import com.example.cashkal.utils.AuthNavigator;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final int MENU_LOGOUT_ID = 1001;
 
     private ActivityHomeBinding binding;
 
@@ -23,5 +28,21 @@ public class HomeActivity extends AppCompatActivity {
                     .replace(R.id.home_fragment_container, new HomeFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, MENU_LOGOUT_ID, Menu.NONE, "התנתקות");
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == MENU_LOGOUT_ID) {
+            AuthNavigator.logout(this);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
